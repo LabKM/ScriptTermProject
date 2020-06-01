0a87fb103bc03fb8c9838c68dbc1321198612f52
Lee Kwan Min <rhksals7864@naver.com>
2020-06-01 14:04:39 +0900

    base

---
+++ ScriptTermProject/ScriptTermProject.py
@@ -0,0 +1,27 @@
+#2020 1학기 스크립트 강의 텀프로젝트 
+#우리동네 국회의원 보기 :  우동국보
+
+#기능 
+# 1. 지역구 입력으로 국회의원 정보 가져오기
+# 2. 국회의원의 최근 활동 보기
+# 3. 국회의원 사무실 위치 보기
+# 4. 국회의원 사무실로 이메일 전송
+# 5. 국회의원 자기 지역구 이외에도 자신이 원하는 국회의원들 관심 등록 기능
+
+from tkinter import *
+
+class Member:
+    def __init__(self):
+        pass
+
+
+class MainApp():
+    def __init__(self, root):
+        pass
+
+
+root = Tk()
+MainApp(root)
+root.mainloop()
+
+
