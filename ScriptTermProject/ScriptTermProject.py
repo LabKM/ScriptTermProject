d83e4d8c0f54f3220eb5f27e786569e6ac7edebe
Lee Kwan Min <rhksals7864@naver.com>
2020-06-04 21:05:22 +0900

    Entry에 지역구 입력시 혹은 일부 지역명 입력시 관련 검색 국회의원을 지역구와 함께 표시

--- ScriptTermProject/ScriptTermProject.py
+++ ScriptTermProject/ScriptTermProject.py
@@ -8,8 +8,10 @@
 # 4. 국회의원 사무실로 이메일 전송
 # 5. 국회의원 자기 지역구 이외에도 자신이 원하는 국회의원들 관심 등록 기능
 
+from tkinter import *
 import urllib
 import requests
+import xml.etree.ElementTree as ET
 
 def get_request_query(url, operation, params, serviceKey):
     import urllib.parse as urlparse
@@ -18,76 +20,61 @@ def get_request_query(url, operation, params, serviceKey):
     return request_query
 
 
-# 요청 URL과 오퍼레이션
-URL = 'http://apis.data.go.kr/9710000/NationalAssemblyInfoService'
-OPERATION = 'getMemberCurrStateList' # 국경일 + 공휴일 정보 조회 오퍼레이션
-
-# 파라미터
-SERVICEKEY = 'vcEXNHaIWXxS5Uz3hGYV%2FQKGjcxzIfqEvuV5Arl0yB66fMYdch6oxV1bMuTLSC7jXzr03Xzt1NkBrDBBzYIe2Q%3D%3D'
-PARAMS = {'numOfRows':'300', 'pageNo':'1'}
-
-request_query = get_request_query(URL, OPERATION, PARAMS, SERVICEKEY)
-response = requests.get(url=request_query)
-
-orig = input("지역구 입력 : ")
-
-import xml.etree.ElementTree as ET
-root = ET.fromstring(response.text)
-
-for item in root.iter('item'):
-    if ( orig == item.find('origNm').text ):
-        for str in item.iter():
-            print(str.text)
+class Member:
+    def __init__(self, item):
+        self.item = item
 
 
-
-
-
-
-#class Member:
-#    def __init__(self, name):
-#        self.name = name
-
-#    def extractBookData(strXml): #strXml은 OpenAPI 검색 결과 XML 문자열
-#        tree = ElementTree.fromstring(strXml)
-#        print (strXml)
-#        # Book 엘리먼트를 가져옵니다.
-#        itemElements = tree.getiterator("item")    # item 엘리먼트 리스트 추출
-#        print(itemElements)
-
-#        for item in itemElements:
-#            isbn = item.find("isbn")          #isbn 검색
-#            strTitle = item.find("title")     #title 검색
-#            print (strTitle)
-#            if len(strTitle.text) > 0 :
-#               return {"ISBN":isbn.text,"title":strTitle.text} # 사전형식 반환
-
-
-#class MainApp():
-#    def __init__(self, root):
-#        self.window = root
-#        self.window.title("우리 동네 국회의원")
-#        self.frames = []
+class MainApp():
+    def __init__(self, root):
+        self.window = root
+        self.window.title("우리 동네 국회의원")
+        self.frame0 = Frame(root)
+        self.frame0.grid(row=0,column=0)
+        self.frames = []
         
-#        self.member_dict = dict()
+        # 요청 URL과 오퍼레이션
+        URL = 'http://apis.data.go.kr/9710000/NationalAssemblyInfoService'
+        OPERATION = 'getMemberCurrStateList' # 국경일 + 공휴일 정보 조회 오퍼레이션
 
-#        self.frames.append(Frame(self.window))
-#        self.frames[0].pack()
-       
-#        self.show_member_name = Label(self.frames[0].)
+        # 파라미터
+        SERVICEKEY = 'vcEXNHaIWXxS5Uz3hGYV%2FQKGjcxzIfqEvuV5Arl0yB66fMYdch6oxV1bMuTLSC7jXzr03Xzt1NkBrDBBzYIe2Q%3D%3D'
+        PARAMS = {'numOfRows':'300', 'pageNo':'1'}
+
+        request_query = get_request_query(URL, OPERATION, PARAMS, SERVICEKEY)
+        response = requests.get(url=request_query)
+
+        self.member_tree = ET.fromstring(response.text).find("body").find("items")
+
+        Button(self.frame0,text="Search",command=lambda X=0: self.pressed(X)).pack(side=LEFT)
+        self.frames.append(Frame(self.window))
+        self.frames[-1].grid(row=1, column=0)
+        self._0_location_member = StringVar()
+        self._0_textbox = Entry(self.frames[-1], textvariable=self._0_location_member)
+        self._0_textbox.grid(row=0,column=0) 
+        self._0_textbox = Button(self.frames[-1], text="검색", command=self.search_member_show_list)
+        self._0_textbox.grid(row=0,column=1) 
+        self._0_listbox = Listbox(self.frames[-1])
+        self._0_listbox.grid(row=1,column=0) 
+
+    def pressed(self, i):
+        self.frames[i].tkraise()
+
+    def show_member(self, name):
+        pass
+
+    def search_member_show_list(self):
+        self._0_listbox.delete(0, END)
+        cnt = 0
+        for item in self.member_tree:
+            origNm = item.find("origNm").text
+            if self._0_location_member.get() in origNm:
+                self._0_listbox.insert(cnt, origNm + ":" + item.find('empNm').text)
+                cnt+=1
         
 
-#    def insert_member(self, info):
-#        self.member_dict[info] = Member(info)
-
-#    def show_member(self, name):
-#        pass
-        
-       
-        
-
-#root = Tk()
-#MainApp(root)
-#root.mainloop()
+root = Tk()
+MainApp(root)
+root.mainloop()
 
 
